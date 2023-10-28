a57a7c3d987489a0d7391901fa35bd5595d5039a
Daniel Leib <leib.mobil@gmail.com>
2023-10-28 11:07:43 +0200

    use rapir for player

--- README.md
+++ README.md
@@ -18,3 +18,64 @@
 - `nix develop` to enter the dev shell (happens automatically when using direnv)
 - `trunk serve` to start the development server
 
+## TODOS
+
+Eigenschaften Kreisel:
+- `v: Vec2` Geschwindigkeit der Position in x und y
+- `a: Vec2` Beschleundigung der Position in x und y
+- `vr: f32` Rotation Speed (Einheit? 0-1 für Prozent)
+- `tilt: Vec2`: Neigung (x und y, 0 - 1)
+- `mass: iu8`: Masse
+- `friction: f32`: Reibung (0 - 1)
+
+Fragen:
+- Rotieren die Spieler Kreisel in eine fest vorgegebene Richtung oder kann die geändert werden. Je nachdem ändert sich das Verhalten bei Kollision.
+
+
+Task 1: Darstellung Kreisel 
+- Drehgeschwindigkeit (Drehendes Sprite, evtl. mit einem schwarzen Punkt am Rand)
+- Neigung (Pfeil)
+- Geschwindigkeit? (zweiter Pfeil, macht es einfacher die Geschwindikeit von Gegnern einzuschätzen)
+- Controls
+- Drehgeschindigkeit (Healthbar über dem Kreisel), sollte für eigenen Kreisel ausblendbar sein
+- Darstellung "Game Over"
+
+Task 2: UI
+- Start Menü
+- Debug Mode: Direkter Start, Slider für die Eigenschaften des Kreisels, ein Gegner der auch über Slider gesteuert werden kann
+- Score (wie wird der berechnet? Nur die Masse für den Anfang?)
+- Geschwindigkeit des eigenen Kreisels als große Healthbar
+
+
+Task 3: Gegner
+- Sollten auch Kreisel sein die in unterschiedlichen Richtungen rotieren
+- Einfärbung je nach Drehrichtung
+- "KI": Nur dumme Kreisel oder sollen diese versuchen den Spieler dumm zu rammen, mit einer Strategie zu rammen
+
+Task 4: Kreiselphysic
+- Wie bewegt sich ein Kreisel ohne Kollision fort?
+- Tilt verhalten
+- Beschleunigungsverhalten
+- Drift verhalten
+
+Task 5: Kollisionen
+- Kollisionen mit statischen Objekten (Wand)
+- Kollisionen mit anderen Kreiseln
+- Wie wirkt sich eine Kollision auf die einzelnen Attribute aus
+- Wenn zwei Kreisel kollidieren, hat der schnellere Kreisel den Vorteil, er kann Masse vom anderen Kreisel anziehen
+
+### Physik
+
+- Eine hohe Masse verlangsamt das Neigung
+- Ein gekippter Kreisel kippt immer weiter
+  - Eine hohe Drehgeschwindgkeit veringert die Neigung
+    - Bei voller `rv` ändert sich die Neigung nicht alleine
+    - Bei einer `rv`  gegen 0 fällt der Kreisel um
+  - Eine hohe Neigung erhöht beschleunigt die Neigung
+    - Um so weiter der Kreisel aus der Balance ist, fällt er leichter um
+- Drift
+  - Eine hohe Drehgeschwindkeit verringert den Drift
+    - Bei voller `rv` gibt es keinen Drift
+  - Eine hohe Neigung erhöht erhöht den Drift
+    - Wenn der Kreisel aufrecht steht gibt es keinen Drift
+- Beschleunigung abhängig von der Neigung und der Masse
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::f32::consts::PI;
+use std::ops::Add;
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 use bevy_rapier2d::prelude::*;
-
 
 const WALL_RADIUS: f32 = 400.;
 const WALL_NUM_SEGMENTS: u8 = 16;
@@ -17,6 +17,7 @@ fn main() {
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
         .add_plugins(RapierDebugRenderPlugin::default())
         .insert_resource(FixedTime::new_from_secs(TIME_STEP))
+        .insert_resource(RapierConfiguration { gravity: Vec2::new(0.0, 0.0), ..default()})
         .add_systems(Startup, setup)
         .add_systems(Startup, setup_physics)
         .add_systems(FixedUpdate, player_movement_system)
@@ -24,9 +25,9 @@ fn main() {
 }
 
 #[derive(Component)]
-struct Player {
-    /// linear speed in meters per second
-    movement_speed: f32,
+struct Spinner {
+    /// tilt in percent (0..1)
+    tilt: Vec2,
     /// rotation speed in radians per second
     rotation_speed: f32,
 }
@@ -39,55 +40,43 @@ fn setup(
     commands.spawn(Camera2dBundle::default());
 
     // Circle
-    commands.spawn(( MaterialMesh2dBundle {
-        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
-        material: materials.add(ColorMaterial::from(Color::PURPLE)),
-        transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
-        ..default()
-    }, Player {
-        movement_speed: 500.0,
-        rotation_speed: f32::to_radians(360.0),
-    } ));
+    // commands.spawn(( MaterialMesh2dBundle {
+    //     mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+    //     material: materials.add(ColorMaterial::from(Color::PURPLE)),
+    //     transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
+    //     ..default()
+    // }, Spinner {
+    //     velocity: Vec2::new(0.0, 0.0),
+    //     rotation_speed: f32::to_radians(360.0),
+    //     tilt: Vec2::new(0.0, 0.0)
+    // } ));
 }
 
 /// Demonstrates applying rotation and movement based on keyboard input.
 fn player_movement_system(
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Player, &mut Transform)>,
+    mut query: Query<(&mut Spinner, &mut Transform, &mut Velocity)>,
 ) {
-    let (ship, mut transform) = query.single_mut();
+    let (mut spinner, mut transform, mut velocity) = query.single_mut();
 
-    let mut rotation_factor = 0.2;
-    let mut movement_factor = 0.5;
+    let mut tilt_x = 0.0;
+    let mut tilt_y = 0.0;
 
     if keyboard_input.pressed(KeyCode::Up) {
-        movement_factor = 1.0;
-        rotation_factor = 0.0;
+        tilt_y += 0.1;
     }
-
-    if keyboard_input.pressed(KeyCode::Left) {
-        rotation_factor += 1.0;
+    if keyboard_input.pressed(KeyCode::Down) {
+        tilt_y -= 0.1;
     }
-
     if keyboard_input.pressed(KeyCode::Right) {
-        rotation_factor -= 1.0;
+        tilt_x += 0.1;
+    }
+    if keyboard_input.pressed(KeyCode::Left) {
+        tilt_x -= 0.1;
     }
 
-    // update the ship rotation around the Z axis (perpendicular to the 2D plane of the screen)
-    transform.rotate_z(rotation_factor * ship.rotation_speed * TIME_STEP);
-
-    // get the ship's forward vector by applying the current rotation to the ships initial facing vector
-    let movement_direction = transform.rotation * Vec3::Y;
-    // get the distance the ship will move based on direction, the ship's movement speed and delta time
-    let movement_distance = movement_factor * ship.movement_speed * TIME_STEP;
-    // create the change in translation using the new movement direction and distance
-    let translation_delta = movement_direction * movement_distance;
-    // update the ship translation with our new translation delta
-    transform.translation += translation_delta;
-
-    // bound the ship within the invisible level bounds
-    let extents = Vec3::from((BOUNDS / 2.0, 0.0));
-    transform.translation = transform.translation.min(extents).max(-extents);
+    spinner.tilt = spinner.tilt.add(Vec2::new(tilt_x, tilt_y)).min(Vec2::ONE).max(Vec2::NEG_ONE);
+    velocity.linvel = velocity.linvel.add(spinner.tilt);
 }
 
 
@@ -110,6 +99,14 @@ fn setup_physics(mut commands: Commands) {
     commands
         .spawn(RigidBody::Dynamic)
         .insert(Collider::ball(50.0))
+        .insert(Spinner {
+            rotation_speed: f32::to_radians(360.0),
+            tilt: Vec2::new(0.0, 0.0)
+        })
         .insert(Restitution::coefficient(0.7))
-        .insert(TransformBundle::from(Transform::from_xyz(100.0, 300.0, 0.0)));
+        .insert(Velocity {
+            linvel: Vec2::ONE,
+            angvel: 0.0
+        })
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 0.0, 0.0)));
 }
